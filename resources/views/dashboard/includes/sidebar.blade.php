3e33bbf5706d25a4d0b8f482d972894ec20a77c0
Ayoub <taknasi.ayoub@gmail.com>
2022-02-05 15:47:36 +0100

    end tags

--- resources/views/dashboard/includes/sidebar.blade.php
+++ resources/views/dashboard/includes/sidebar.blade.php
@@ -69,6 +69,21 @@
                 </ul>
             </li>
 
+            <li class="nav-item"><a href=""><i class="la la-group"></i>
+                    <span class="menu-title" data-i18n="nav.dash.main">الكلمات الدلالية  </span>
+                    <span
+                        class="badge badge badge-danger badge-pill float-right mr-2"></span>
+                </a>
+                <ul class="menu-content">
+                    <li class="active"><a class="menu-item" href="{{ route('tags.index') }}"
+                                          data-i18n="nav.dash.ecommerce"> عرض الكل </a>
+                    </li>
+                    <li><a class="menu-item" href="{{ route('tags.create') }}" data-i18n="nav.dash.crypto">أضافة
+                     كلمة دلالية جديد </a>
+                    </li>
+                </ul>
+            </li>
+
             <li class="nav-item"><a href=""><i class="la la-male"></i>
                     <span class="menu-title" data-i18n="nav.dash.main">المتاجر  </span>
                     <span
